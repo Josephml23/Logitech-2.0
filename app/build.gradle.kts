1118dcafb06b7abcba3e0e9dff48326a7cbccfc0
diegoaragonmurillo-sys <diego.aragon@tecsup.edu.pe>
2026-05-20 17:47:37 -0500

    Scanner completo con CameraX, QR y OCR en carpeta correcta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,10 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation("androidx.camera:camera-core:1.3.1")
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("androidx.camera:camera-view:1.3.1")
+    implementation("com.google.mlkit:text-recognition:16.0.0")
+    implementation("com.google.mlkit:barcode-scanning:17.2.0")
 }
