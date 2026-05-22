2756d19df06c910aacca6c307322bff2edfd1da1
willytorres3001 <willy.torres@tecsup.edu.pe>
2026-05-22 11:14:46 -0500

    Integración completa de scanner OCR y QR
    Se integró CameraX con ML Kit OCR y Barcode Scanning en tiempo real.

--- app/src/main/java/com/example/logist_tech/MainActivity.kt
+++ app/src/main/java/com/example/logist_tech/MainActivity.kt
@@ -5,29 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.logist_tech.scanner.CameraScreen
+import com.example.logist_tech.scanner.ScannerScreen
 import com.example.logist_tech.ui.theme.Logist_TechTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
 
         setContent {
+
             Logist_TechTheme {
 
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
+                ) {
 
-                    CameraScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ScannerScreen()
 
                 }
             }
